dafd1e05f3e8958da6ed3f665f0ff225596f8fc1
Ivan Demchenkov <ivan@oktopost.com>
2017-07-27 09:42:37 +0300

    Added name() to IHandlerConfig

--- src/Eddy/IHandlerConfig.php
+++ src/Eddy/IHandlerConfig.php
@@ -4,6 +4,7 @@ namespace Eddy;
 
 interface IHandlerConfig
 {
+	public function name(): string;
 	public function delay(): float;
 	public function initialState(): string;
 	public function handlerClassName(): string;
