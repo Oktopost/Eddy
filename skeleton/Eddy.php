020dc707c2d3cd8a8b317df7b4b6360fce2fded7
Ivan Demchenkov <ivan@oktopost.com>
2017-08-06 16:51:26 +0300

    Move Modules skeleton config to separate file

--- skeleton/Eddy.php
+++ skeleton/Eddy.php
@@ -49,21 +49,3 @@ $this->set(IStatisticsStorage::class, MySQLStatsStorage::class);
 $this->set(IStatisticsCacheCollector::class, RedisStatsCacheCollectorTest::class);
 $this->set(IStatisticsCollectionDecorator::class, StatisticsCollectionDecorator::class);
 $this->set(IProcessStatistics::class, ProcessStatistics::class);
-
-
-use Eddy\Base\Module\ISetupModule;
-use Eddy\Base\Module\IEventModule;
-use Eddy\Base\Module\IHandlersModule;
-use Eddy\Base\Module\IEddyObjectModule;
-use Eddy\Base\Module\ISubscribersModule;
-use Eddy\Modules\SetupModule;
-use Eddy\Modules\EventModule;
-use Eddy\Modules\HandlersModule;
-use Eddy\Modules\EddyObjectModule;
-use Eddy\Modules\SubscribersModule;
-
-$this->set(IEventModule::class, EventModule::class);
-$this->set(ISetupModule::class, SetupModule::class);
-$this->set(IHandlersModule::class, HandlersModule::class);
-$this->set(IEddyObjectModule::class, EddyObjectModule::class);
-$this->set(ISubscribersModule::class, SubscribersModule::class);
