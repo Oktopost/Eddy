1ab20c27ca6b25bc1a67a8fec1ae30be29a91aa3
Ivan Demchenkov <ivan@oktopost.com>
2017-08-09 15:10:34 +0300

    Added flushAll method to CachedDAL

--- src/Eddy/DAL/CachedDAL.php
+++ src/Eddy/DAL/CachedDAL.php
@@ -16,20 +16,20 @@ class CachedDAL implements IDAL
 {
 	/** @var IDAL */
 	private $main;
-	
+
 	/** @var IDAL */
 	private $cache;
-	
+
 	/** @var IHandlerDAO */
 	private $handlerDAO;
-	
+
 	/** @var IEventDAO */
 	private $eventDAO;
-	
+
 	/** @var ISubscribersDAO */
 	private $subscribersDAO;
-	
-	
+
+
 	public function __construct(IDAL $mainDAL, IDAL $cacheDAL)
 	{
 		$this->main = $mainDAL;
@@ -39,37 +39,50 @@ class CachedDAL implements IDAL
 
 	public function handlers(): IHandlerDAO
 	{
-		if (!$this->handlerDAO)
-		{
+		if (!$this->handlerDAO) {
 			$this->handlerDAO = Scope::skeleton(ICachedHandlerDAO::class);
 			$this->handlerDAO->setMain($this->main->handlers());
 			$this->handlerDAO->setCache($this->cache->handlers());
 		}
-		
+
 		return $this->handlerDAO;
 	}
 
 	public function events(): IEventDAO
 	{
-		if (!$this->eventDAO)
-		{
+		if (!$this->eventDAO) {
 			$this->eventDAO = Scope::skeleton(ICachedEventDAO::class);
 			$this->eventDAO->setMain($this->main->events());
 			$this->eventDAO->setCache($this->cache->events());
 		}
-		
+
 		return $this->eventDAO;
 	}
 
 	public function subscribers(): ISubscribersDAO
 	{
-		if (!$this->subscribersDAO)
-		{
+		if (!$this->subscribersDAO) {
 			$this->subscribersDAO = Scope::skeleton(ICachedSubscribersDAO::class);
 			$this->subscribersDAO->setMain($this->main->subscribers());
 			$this->subscribersDAO->setCache($this->cache->subscribers());
 		}
-		
+
 		return $this->subscribersDAO;
+	}
+
+	public function flushCache(): void
+	{
+		/** @var ICachedSubscribersDAO $subscribers */
+		$subscribers = $this->subscribers();
+		
+		/** @var ICachedHandlerDAO $handlers */
+		$handlers = $this->handlers();
+		
+		/** @var ICachedEventDAO $events */
+		$events = $this->events();
+		
+		$subscribers->flushAll();
+		$events->flushAll();
+		$handlers->flushAll();
 	}
 }
--- tests/Eddy/DAL/CachedDALTest.php
+++ tests/Eddy/DAL/CachedDALTest.php
@@ -20,6 +20,13 @@ class CachedDALTest extends TestCase
 		return new CachedDAL(new MySQLDAL(MySQLConfig::connector()), new RedisDAL(new Client([])));
 	}
 	
+	
+	protected function setUp()
+	{
+		\UnitTestScope::clear();
+	}
+
+
 	public function test_handlers_gotIHandlerDAO()
 	{
 		self::assertInstanceOf(ICachedHandlerDAO::class, $this->getSubject()->handlers());
@@ -30,8 +37,26 @@ class CachedDALTest extends TestCase
 		self::assertInstanceOf(ICachedEventDAO::class, $this->getSubject()->events());
 	}
 	
-	public function test_subscrivers_gotISubscribersDAO()
+	public function test_subscribers_gotISubscribersDAO()
 	{
 		self::assertInstanceOf(ICachedSubscribersDAO::class, $this->getSubject()->subscribers());
+	}
+	
+	public function test_flushCache()
+	{
+		$subscribersMock = $this->getMockBuilder(ICachedSubscribersDAO::class)->getMock();
+		$subscribersMock->expects($this->once())->method('flushAll');
+		
+		$handlersMock = $this->getMockBuilder(ICachedHandlerDAO::class)->getMock();
+		$handlersMock->expects($this->once())->method('flushAll');
+		
+		$eventsMock = $this->getMockBuilder(ICachedEventDAO::class)->getMock();
+		$eventsMock->expects($this->once())->method('flushAll');
+		
+		\UnitTestScope::override(ICachedSubscribersDAO::class, $subscribersMock);
+		\UnitTestScope::override(ICachedHandlerDAO::class, $handlersMock);
+		\UnitTestScope::override(ICachedEventDAO::class, $eventsMock);
+		
+		$this->getSubject()->flushCache();
 	}
 }
