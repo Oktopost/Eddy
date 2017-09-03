5333f3166905f446be504f1f7d5cc1b98da30e52
Daniel Podrabinek <dpodrabinek@oktopost.com>
2017-09-03 10:11:34 +0300

    Added AbstractHandlerConfig

---
+++ src/Eddy/Handler/AbstractHandlerConfig.php
@@ -0,0 +1,25 @@
+<?php
+namespace Eddy\Handler;
+
+
+use Eddy\IHandlerConfig;
+use Eddy\Enums\EventState;
+
+use Annotation\Value;
+
+
+class AbstractHandlerConfig implements IHandlerConfig
+{
+	public function name(): string
+	{
+		return Value::getValue($this, 'name');
+	}
+	
+	public function delay(): float				{ return 0; }
+	public function maxBulkSize(): int			{ return 256; }
+	public function initialState(): string 		{ return EventState::PAUSED; }
+	public function handlerClassName(): string	{ return static::class; }
+	public function getInstance()				{ return new static(); }
+	public function delayBuffer(): float		{ return 0; }
+	public function packageSize(): int			{ return 0; }
+}
